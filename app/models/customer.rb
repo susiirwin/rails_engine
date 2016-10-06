d2b1d451561c37d0a899aa6f460297a6eab32404
Daniel Broadbent <dbroadbent85@gmail.com>
2016-10-06 17:07:57 -0600

    adds customer favorite merchant business intellegence

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,19 @@ class Customer < ApplicationRecord
   has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
 
+  def result
+    transactions.select('invoices.merchant_id, COUNT(transactions.id) AS transactions_count')
+                     .merge(Transaction.success)
+                     .group('invoices.merchant_id')
+                     .order('transactions_count DESC')
+                     .limit(1)
+  end
+
+  def merchant_id
+    result[0]['merchant_id']
+  end
+
   def favorite_merchant
+    Merchant.find(merchant_id)
   end
 end
